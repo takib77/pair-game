73f476503626489fc049553e0ba107bfdf6218ab
Leo <takib77@gmail.com>
2020-12-03 16:01:11 +0100

    Cards bg image + timer

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+const timer = () => {
+    let stopperTime = 0;
+    setInterval(() => {
+        stopperTime++;
+        const seconds = zeroForTime(stopperTime % 60);
+        const minutes = zeroForTime(Math.floor(stopperTime / 60) % 60);
+        const time = `${[minutes, seconds].join(':')}`
+        const showTime = document.querySelector('.timer');
+        showTime.textContent = time;
+    }, 1000);
+}
+
+const zeroForTime = (number) => {
+    return number < 10 ? `0${number}` : `${number}`;
+}
+
+timer();
